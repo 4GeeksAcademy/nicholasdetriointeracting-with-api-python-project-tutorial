2cf45f765f9bfc9962f6fd57567a060898eec9a9
Nicholas Detrio <56412393+nicholasdetrio@users.noreply.github.com>
2024-03-09 02:10:38 +0000

    ND API Exercise final commit

--- src/app.py
+++ src/app.py
@@ -2,12 +2,19 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # load the .env file variables
 load_dotenv()
 
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
+con = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret))
+print(con)
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
